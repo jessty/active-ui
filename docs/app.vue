f2d3b68f36c799e77f6e40fe867e7cc4a58e8797
IvanWarke <iveon.cc@gmail.com>
2017-05-22 20:08:30 +0800

    css reset

--- docs/app.vue
+++ docs/app.vue
@@ -6,7 +6,7 @@ nav { margin-bottom: 40px; }
 ul { display: flex; flex-wrap: wrap; }
 li { display: inline-block; }
 li + li { border-left: solid 1px #bbb; padding-left: 10px; margin-left: 10px; }
-.container { padding: 10px 40px; }
+.container{ padding: 10px 40px; }
 .v-link-active { color: #bbb; }
 </style>
 
